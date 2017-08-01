7b48735309d154f43304f7b00520689a8aa77e98
Sandipan Dutta <sandy9091809@gmail.com>
2017-08-01 17:56:11 +0530

    Changes in scrpit.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $("#btn1").click(function(){
  $.ajax({
-                url: 'https://staging.letzchange.org/search?fq=(type:campaign)&row=10',
+                url: 'https://letzchange.org/search?fq=(type:campaign)&row=10',
                 type: 'GET',
                 dataType: 'json',
                 success: function (data, textStatus, xhr) {
