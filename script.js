0f66f28f10cf3261257db6a8de0dbbfb4d645b5b
Sandipan Dutta <sandy9091809@gmail.com>
2017-08-02 17:35:17 +0530

    Dynamically creating card

--- script.js
+++ script.js
@@ -1,66 +1,37 @@
 $("#btn1").click(function(){
-//  $.ajax({
-//                 url: 'https://staging.letzchange.org/search?fq=(type:campaign)&row=10',
-//                 type: 'GET',
-//                 dataType: 'json',
-//                 success: function (data, textStatus, xhr) {
-//                     console.log(data);
-//                 },
-//                 error: function (xhr, textStatus, errorThrown) {
-//                     console.log('Error in Database');
-//                 }
-//             });
-
-    $.fn.getCampaigns("nonprofit");
-    // $.fn.getNonProfit();
-    // $.fn.getProjects();
-
-});
-
+    $.fn.getCampaigns("campaign");
+})
 $(document).ready(function(){
     
    $.fn.getCampaigns = function(x){
        $.ajax({
-                url: 'https://letzchange.org/search?fq=(type:'+ x + ')&row=10',
+                url: 'https://staging.letzchange.org/search?fq=(type:'+ x + ')&row=10',
                 type: 'GET',
                 dataType: 'json',
                 success: function (data, textStatus, xhr) {
-                    console.log("Getting" + x);
-                    console.log(data);
+                    console.log("Getting " + x);
+                    // console.log(data.response.docs);
+
+                    data.response.docs.forEach(function(d) {
+                         
+                         console.log(d.name);
+                           card = document.createElement('div');
+                           card.className = 'card';
+                           card.id = "CARD"
+                        //    card.innerHTML = "data"
+                        document.getElementById("main").appendChild(card);
+
+                        titlewrapper = document.createElement('div');
+                        titlewrapper.className = 'title-wrapper';
+                        titlewrapper.id = 'Title';
+                        document.getElementById("CARD").appendChild(titlewrapper);
+                        
+                    });
+
                 },
                 error: function (xhr, textStatus, errorThrown) {
                     console.log('Error in Database');
                 }
-            });
+            })
    }
-
-//    $.fn.getNonProfit = function(){
-//        $.ajax({
-//                 url: 'https://letzchange.org/search?fq=(type:nonprofit)&row=10',
-//                 type: 'GET',
-//                 dataType: 'json',
-//                 success: function (data, textStatus, xhr) {
-//                     console.log("Getting Non-Profits!");
-//                     console.log(data);
-//                 },
-//                 error: function (xhr, textStatus, errorThrown) {
-//                     console.log('Error in Database');
-//                 }
-//             });
-//    }
-
-//    $.fn.getProjects = function(){
-//        $.ajax({
-//                 url: 'https://letzchange.org/search?fq=(type:project)&row=10',
-//                 type: 'GET',
-//                 dataType: 'json',
-//                 success: function (data, textStatus, xhr) {
-//                     console.log("Getting projects!");
-//                     console.log(data);
-//                 },
-//                 error: function (xhr, textStatus, errorThrown) {
-//                     console.log('Error in Database');
-//                 }
-//             });
-//    }
-});
+   })
