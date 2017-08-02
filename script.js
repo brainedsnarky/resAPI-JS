6f43670780198fd014f83dfffe1d494af66a1d97
Sandipan Dutta <sandy9091809@gmail.com>
2017-08-02 15:24:44 +0530

    Function with params

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $("#btn1").click(function(){
 //  $.ajax({
-//                 url: 'https://letzchange.org/search?fq=(type:campaign)&row=10',
+//                 url: 'https://staging.letzchange.org/search?fq=(type:campaign)&row=10',
 //                 type: 'GET',
 //                 dataType: 'json',
 //                 success: function (data, textStatus, xhr) {
@@ -10,20 +10,22 @@ $("#btn1").click(function(){
 //                     console.log('Error in Database');
 //                 }
 //             });
-    $.fn.getCampaigns();
-    $.fn.getNonProfit();
-    $.fn.getProjects();
+
+    $.fn.getCampaigns("nonprofit");
+    // $.fn.getNonProfit();
+    // $.fn.getProjects();
+
 });
 
 $(document).ready(function(){
     
-   $.fn.getCampaigns = function(){
+   $.fn.getCampaigns = function(x){
        $.ajax({
-                url: 'https://letzchange.org/search?fq=(type:campaign)&row=10',
+                url: 'https://letzchange.org/search?fq=(type:'+ x + ')&row=10',
                 type: 'GET',
                 dataType: 'json',
                 success: function (data, textStatus, xhr) {
-                    console.log("Getting campaigns!");
+                    console.log("Getting" + x);
                     console.log(data);
                 },
                 error: function (xhr, textStatus, errorThrown) {
@@ -32,33 +34,33 @@ $(document).ready(function(){
             });
    }
 
-   $.fn.getNonProfit = function(){
-       $.ajax({
-                url: 'https://letzchange.org/search?fq=(type:nonprofit)&row=10',
-                type: 'GET',
-                dataType: 'json',
-                success: function (data, textStatus, xhr) {
-                    console.log("Getting Non-Profits!");
-                    console.log(data);
-                },
-                error: function (xhr, textStatus, errorThrown) {
-                    console.log('Error in Database');
-                }
-            });
-   }
+//    $.fn.getNonProfit = function(){
+//        $.ajax({
+//                 url: 'https://letzchange.org/search?fq=(type:nonprofit)&row=10',
+//                 type: 'GET',
+//                 dataType: 'json',
+//                 success: function (data, textStatus, xhr) {
+//                     console.log("Getting Non-Profits!");
+//                     console.log(data);
+//                 },
+//                 error: function (xhr, textStatus, errorThrown) {
+//                     console.log('Error in Database');
+//                 }
+//             });
+//    }
 
-   $.fn.getProjects = function(){
-       $.ajax({
-                url: 'https://letzchange.org/search?fq=(type:project)&row=10',
-                type: 'GET',
-                dataType: 'json',
-                success: function (data, textStatus, xhr) {
-                    console.log("Getting projects!");
-                    console.log(data);
-                },
-                error: function (xhr, textStatus, errorThrown) {
-                    console.log('Error in Database');
-                }
-            });
-   }
+//    $.fn.getProjects = function(){
+//        $.ajax({
+//                 url: 'https://letzchange.org/search?fq=(type:project)&row=10',
+//                 type: 'GET',
+//                 dataType: 'json',
+//                 success: function (data, textStatus, xhr) {
+//                     console.log("Getting projects!");
+//                     console.log(data);
+//                 },
+//                 error: function (xhr, textStatus, errorThrown) {
+//                     console.log('Error in Database');
+//                 }
+//             });
+//    }
 });
