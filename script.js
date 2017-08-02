e6d557260a605ef4baff2a62ae7054e404752fcb
Sandipan Dutta <sandy9091809@gmail.com>
2017-08-02 13:15:53 +0530

    Changes in scrpit.jsD

--- script.js
+++ script.js
@@ -9,5 +9,50 @@ $("#btn1").click(function(){
                 error: function (xhr, textStatus, errorThrown) {
                     console.log('Error in Database');
                 }
-            })
-})
+            });
+});
+
+$(document).ready(function(){
+    
+   $.fn.getCampaigns = function(){
+       $.ajax({
+                url: 'https://letzchange.org/search?fq=(type:campaign)&row=10',
+                type: 'GET',
+                dataType: 'json',
+                success: function (data, textStatus, xhr) {
+                    console.log(data);
+                },
+                error: function (xhr, textStatus, errorThrown) {
+                    console.log('Error in Database');
+                }
+            });
+   }
+
+   $.fn.getNonProfit = function(){
+       $.ajax({
+                url: 'https://letzchange.org/search?fq=(type:nonprofit)&row=10',
+                type: 'GET',
+                dataType: 'json',
+                success: function (data, textStatus, xhr) {
+                    console.log(data);
+                },
+                error: function (xhr, textStatus, errorThrown) {
+                    console.log('Error in Database');
+                }
+            });
+   }
+
+   $.fn.getProjects = function(){
+       $.ajax({
+                url: 'https://letzchange.org/search?fq=(type:project)&row=10',
+                type: 'GET',
+                dataType: 'json',
+                success: function (data, textStatus, xhr) {
+                    console.log(data);
+                },
+                error: function (xhr, textStatus, errorThrown) {
+                    console.log('Error in Database');
+                }
+            });
+   }
+});
