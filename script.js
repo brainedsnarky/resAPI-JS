676ac455815e8bc33bddb39ab6a888cd364e3dc0
Sandipan Dutta <sandy9091809@gmail.com>
2017-08-03 14:29:57 +0530

    Added Filters

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 $("#btn1").click(function(){
     $.fn.getCampaigns("campaign");
 })
+
+$("#supporters_count").click(function(){
+    $.fn.BySupportersCount("campaign");
+})
+
+$("#amnt_raised").click(function(){
+    $.fn.ByAmountRaised("campaign");
+})
+
+$("#date_modified").click(function(){
+    $.fn.ByDateModified("campaign");
+})
+
+
 $(document).ready(function(){
     
    $.fn.getCampaigns = function(x){
@@ -37,6 +51,7 @@ $(document).ready(function(){
                             card.appendChild(contentwrapper);
 
                             document.getElementById("main").appendChild(card);
+
                     });
             },
                 error: function (xhr, textStatus, errorThrown) {
@@ -44,4 +59,134 @@ $(document).ready(function(){
                 }
             });
    }
+
+   $.fn.BySupportersCount = function(x){
+              $.ajax({
+                url: 'https://staging.letzchange.org/search?fq=(type:'+ x +')&sort=supporter_count%20desc&row=10',
+                type: 'GET',
+                dataType: 'json',
+                success: function (data, textStatus, xhr) {
+                    data.response.docs.forEach(function(d,i) {
+                            card = document.createElement('div');
+                            card.className = 'card';
+                            card.id = "CARD"+i;
+
+                            titlewrapper = document.createElement('div');
+                            titlewrapper.className = 'title-wrapper';
+                            titlewrapper.id = "Title"+i;
+                            titlewrapper.innerHTML = d.name;
+                            card.appendChild(titlewrapper);
+
+                            imgwrapper = document.createElement('div');
+                            imgwrapper.className = 'image-wrapper';
+                            imgwrapper.id = "Image"+i;
+                            card.appendChild(imgwrapper);
+
+                            img = document.createElement('img');
+                            img.setAttribute("alt","Image");
+                            img.setAttribute("src", d.creator_image);
+                            imgwrapper.appendChild(img);
+
+                            contentwrapper = document.createElement('div');
+                            contentwrapper.className = 'content-wrapper';
+                            contentwrapper.id = "Content"+i;
+                            contentwrapper.innerHTML = d.description;
+                            card.appendChild(contentwrapper);
+
+                            document.getElementById("main").appendChild(card);
+
+                    });
+            },
+                error: function (xhr, textStatus, errorThrown) {
+                    console.log('Error in Database');
+                }
+            });
+   }
+
+   $.fn.ByAmountRaised = function(x){
+              $.ajax({
+                url: 'https://staging.letzchange.org/search?fq=(type:'+ x +')&sort=raised_amount_in_inr%20desc&row=10',
+                type: 'GET',
+                dataType: 'json',
+                success: function (data, textStatus, xhr) {
+                    data.response.docs.forEach(function(d,i) {
+                            card = document.createElement('div');
+                            card.className = 'card';
+                            card.id = "CARD"+i;
+
+                            titlewrapper = document.createElement('div');
+                            titlewrapper.className = 'title-wrapper';
+                            titlewrapper.id = "Title"+i;
+                            titlewrapper.innerHTML = d.name;
+                            card.appendChild(titlewrapper);
+
+                            imgwrapper = document.createElement('div');
+                            imgwrapper.className = 'image-wrapper';
+                            imgwrapper.id = "Image"+i;
+                            card.appendChild(imgwrapper);
+
+                            img = document.createElement('img');
+                            img.setAttribute("alt","Image");
+                            img.setAttribute("src", d.creator_image);
+                            imgwrapper.appendChild(img);
+
+                            contentwrapper = document.createElement('div');
+                            contentwrapper.className = 'content-wrapper';
+                            contentwrapper.id = "Content"+i;
+                            contentwrapper.innerHTML = d.description;
+                            card.appendChild(contentwrapper);
+
+                            document.getElementById("main").appendChild(card);
+
+                    });
+            },
+                error: function (xhr, textStatus, errorThrown) {
+                    console.log('Error in Database');
+                }
+            });
+   }
+
+      $.fn.ByDateModified = function(x){
+              $.ajax({
+                url: 'https://staging.letzchange.org/search?fq=(type:'+ x +')&sort=created_at%20asc&row=10',
+                type: 'GET',
+                dataType: 'json',
+                success: function (data, textStatus, xhr) {
+                    data.response.docs.forEach(function(d,i) {
+                            card = document.createElement('div');
+                            card.className = 'card';
+                            card.id = "CARD"+i;
+
+                            titlewrapper = document.createElement('div');
+                            titlewrapper.className = 'title-wrapper';
+                            titlewrapper.id = "Title"+i;
+                            titlewrapper.innerHTML = d.name;
+                            card.appendChild(titlewrapper);
+
+                            imgwrapper = document.createElement('div');
+                            imgwrapper.className = 'image-wrapper';
+                            imgwrapper.id = "Image"+i;
+                            card.appendChild(imgwrapper);
+
+                            img = document.createElement('img');
+                            img.setAttribute("alt","Image");
+                            img.setAttribute("src", d.creator_image);
+                            imgwrapper.appendChild(img);
+
+                            contentwrapper = document.createElement('div');
+                            contentwrapper.className = 'content-wrapper';
+                            contentwrapper.id = "Content"+i;
+                            contentwrapper.innerHTML = d.description;
+                            card.appendChild(contentwrapper);
+
+                            document.getElementById("main").appendChild(card);
+
+                    });
+            },
+                error: function (xhr, textStatus, errorThrown) {
+                    console.log('Error in Database');
+                }
+            });
+   }
+
 });
