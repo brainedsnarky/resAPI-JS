d272d8aae10c632500021c0286bfa425ec182753
Sandipan Dutta <sandy9091809@gmail.com>
2017-08-03 12:16:51 +0530

    Iterating over data through dynamic html added

--- script.js
+++ script.js
@@ -9,29 +9,39 @@ $(document).ready(function(){
                 type: 'GET',
                 dataType: 'json',
                 success: function (data, textStatus, xhr) {
-                    console.log("Getting " + x);
-                    // console.log(data.response.docs);
+                    data.response.docs.forEach(function(d,i) {
+                            card = document.createElement('div');
+                            card.className = 'card';
+                            card.id = "CARD"+i;
 
-                    data.response.docs.forEach(function(d) {
-                         
-                         console.log(d.name);
-                           card = document.createElement('div');
-                           card.className = 'card';
-                           card.id = "CARD"
-                        //    card.innerHTML = "data"
-                        document.getElementById("main").appendChild(card);
+                            titlewrapper = document.createElement('div');
+                            titlewrapper.className = 'title-wrapper';
+                            titlewrapper.id = "Title"+i;
+                            titlewrapper.innerHTML = d.name;
+                            card.appendChild(titlewrapper);
 
-                        titlewrapper = document.createElement('div');
-                        titlewrapper.className = 'title-wrapper';
-                        titlewrapper.id = 'Title';
-                        document.getElementById("CARD").appendChild(titlewrapper);
-                        
+                            imgwrapper = document.createElement('div');
+                            imgwrapper.className = 'image-wrapper';
+                            imgwrapper.id = "Image"+i;
+                            card.appendChild(imgwrapper);
+
+                            img = document.createElement('img');
+                            img.setAttribute("alt","Image");
+                            img.setAttribute("src", d.creator_image);
+                            imgwrapper.appendChild(img);
+
+                            contentwrapper = document.createElement('div');
+                            contentwrapper.className = 'content-wrapper';
+                            contentwrapper.id = "Content"+i;
+                            contentwrapper.innerHTML = d.description;
+                            card.appendChild(contentwrapper);
+
+                            document.getElementById("main").appendChild(card);
                     });
-
-                },
+            },
                 error: function (xhr, textStatus, errorThrown) {
                     console.log('Error in Database');
                 }
-            })
+            });
    }
-   })
+   });
