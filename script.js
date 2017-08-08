65b6af8bf40ff79c01efdd12741b1bd19556a7bd
Sandipan Dutta <sandy9091809@gmail.com>
2017-08-08 14:10:28 +0530

    Advanced search js code addedgit status

--- script.js
+++ script.js
@@ -408,4 +408,225 @@ $(document).ready(function(){
                });
    }
 
+   $.fn.AdvancedSearch = function(y){
+       $.ajax({
+                url: 'https://staging.letzchange.org/search?fq=(type:campaign%20AND%20parent_name'+ y +')&row=10',
+                type: 'GET',
+                dataType: 'json',
+                success: function (data, textStatus, xhr) {
+                    console.log(data.response.docs);
+                    data.response.docs.forEach(function(d,i) {
+                            card = document.createElement('div');
+                            card.className = 'card';
+                            card.id = "CARD"+i;
+
+                            titlewrapper = document.createElement('div');
+                            titlewrapper.className = 'title-wrapper';
+                            titlewrapper.id = "Title"+i;
+                            titlewrapper.innerHTML = d.name;
+                            card.appendChild(titlewrapper);
+
+                            imgwrapper = document.createElement('div');
+                            imgwrapper.className = 'image-wrapper';
+                            imgwrapper.id = "Image"+i;
+                            card.appendChild(imgwrapper);
+
+                            img = document.createElement('img');
+                            img.setAttribute("alt","Image");
+                            img.setAttribute("src", d.creator_image);
+                            imgwrapper.appendChild(img);
+
+                            stat = document.createElement('div');
+                            stat.className ='stats-wrapper';
+                            stat.id ="Stats"+i;
+
+                            fadate = document.createElement('i');
+                            fadate.className = 'fa fa-clock-o';
+
+                            date = document.createElement('p');
+                            date.id = "date"+i;
+                            date.innerHTML = (new Date(d.created_at)).toDateString();
+                            fadate.appendChild(date);
+
+                            fasupp = document.createElement('i');
+                            fasupp.className = 'fa fa-heart';
+
+                            count = document.createElement('p');
+                            count.id = "count"+i;
+                            count.innerHTML = d.supporter_count;
+                            fasupp.appendChild(count);
+
+                            amount = document.createElement('p');
+                            amount.id = "amount"+i;
+                            amount.innerHTML = d.raised_amount_in_inr;
+
+                            stat.appendChild(fadate);
+                            stat.appendChild(fasupp);
+                            stat.appendChild(amount);
+
+                            card.appendChild(stat);
+
+                            contentwrapper = document.createElement('div');
+                            contentwrapper.className = 'content-wrapper';
+                            contentwrapper.id = "Content"+i;
+                            contentwrapper.innerHTML = d.description;
+                            card.appendChild(contentwrapper);
+
+                            document.getElementById("main").appendChild(card);
+                            document.getElementById("btn1").disabled = true;
+
+                    });
+                },
+                error: function (xhr, textStatus, errorThrown) {
+                    console.log('Error in Database');
+                }
+               });
+
+               $.ajax({
+                url: 'https://staging.letzchange.org/search?fq=(type:campaign%20AND%20creator_name'+ y +')&row=10',
+                type: 'GET',
+                dataType: 'json',
+                success: function (data, textStatus, xhr) {
+                    console.log(data.response.docs);
+                    data.response.docs.forEach(function(d,i) {
+                            card = document.createElement('div');
+                            card.className = 'card';
+                            card.id = "CARD"+i;
+
+                            titlewrapper = document.createElement('div');
+                            titlewrapper.className = 'title-wrapper';
+                            titlewrapper.id = "Title"+i;
+                            titlewrapper.innerHTML = d.name;
+                            card.appendChild(titlewrapper);
+
+                            imgwrapper = document.createElement('div');
+                            imgwrapper.className = 'image-wrapper';
+                            imgwrapper.id = "Image"+i;
+                            card.appendChild(imgwrapper);
+
+                            img = document.createElement('img');
+                            img.setAttribute("alt","Image");
+                            img.setAttribute("src", d.creator_image);
+                            imgwrapper.appendChild(img);
+
+                            stat = document.createElement('div');
+                            stat.className ='stats-wrapper';
+                            stat.id ="Stats"+i;
+
+                            fadate = document.createElement('i');
+                            fadate.className = 'fa fa-clock-o';
+
+                            date = document.createElement('p');
+                            date.id = "date"+i;
+                            date.innerHTML = (new Date(d.created_at)).toDateString();
+                            fadate.appendChild(date);
+
+                            fasupp = document.createElement('i');
+                            fasupp.className = 'fa fa-heart';
+
+                            count = document.createElement('p');
+                            count.id = "count"+i;
+                            count.innerHTML = d.supporter_count;
+                            fasupp.appendChild(count);
+
+                            amount = document.createElement('p');
+                            amount.id = "amount"+i;
+                            amount.innerHTML = d.raised_amount_in_inr;
+
+                            stat.appendChild(fadate);
+                            stat.appendChild(fasupp);
+                            stat.appendChild(amount);
+
+                            card.appendChild(stat);
+
+                            contentwrapper = document.createElement('div');
+                            contentwrapper.className = 'content-wrapper';
+                            contentwrapper.id = "Content"+i;
+                            contentwrapper.innerHTML = d.description;
+                            card.appendChild(contentwrapper);
+
+                            document.getElementById("main").appendChild(card);
+                            document.getElementById("btn1").disabled = true;
+
+                    });
+                },
+                error: function (xhr, textStatus, errorThrown) {
+                    console.log('Error in Database');
+                }
+               });
+
+               $.ajax({
+                url: 'https://staging.letzchange.org/search?fq=(type:campaign%20AND%20region'+ y +')&row=10',
+                type: 'GET',
+                dataType: 'json',
+                success: function (data, textStatus, xhr) {
+                    console.log(data.response.docs);
+                    data.response.docs.forEach(function(d,i) {
+                            card = document.createElement('div');
+                            card.className = 'card';
+                            card.id = "CARD"+i;
+
+                            titlewrapper = document.createElement('div');
+                            titlewrapper.className = 'title-wrapper';
+                            titlewrapper.id = "Title"+i;
+                            titlewrapper.innerHTML = d.name;
+                            card.appendChild(titlewrapper);
+
+                            imgwrapper = document.createElement('div');
+                            imgwrapper.className = 'image-wrapper';
+                            imgwrapper.id = "Image"+i;
+                            card.appendChild(imgwrapper);
+
+                            img = document.createElement('img');
+                            img.setAttribute("alt","Image");
+                            img.setAttribute("src", d.creator_image);
+                            imgwrapper.appendChild(img);
+
+                            stat = document.createElement('div');
+                            stat.className ='stats-wrapper';
+                            stat.id ="Stats"+i;
+
+                            fadate = document.createElement('i');
+                            fadate.className = 'fa fa-clock-o';
+
+                            date = document.createElement('p');
+                            date.id = "date"+i;
+                            date.innerHTML = (new Date(d.created_at)).toDateString();
+                            fadate.appendChild(date);
+
+                            fasupp = document.createElement('i');
+                            fasupp.className = 'fa fa-heart';
+
+                            count = document.createElement('p');
+                            count.id = "count"+i;
+                            count.innerHTML = d.supporter_count;
+                            fasupp.appendChild(count);
+
+                            amount = document.createElement('p');
+                            amount.id = "amount"+i;
+                            amount.innerHTML = d.raised_amount_in_inr;
+
+                            stat.appendChild(fadate);
+                            stat.appendChild(fasupp);
+                            stat.appendChild(amount);
+
+                            card.appendChild(stat);
+
+                            contentwrapper = document.createElement('div');
+                            contentwrapper.className = 'content-wrapper';
+                            contentwrapper.id = "Content"+i;
+                            contentwrapper.innerHTML = d.description;
+                            card.appendChild(contentwrapper);
+
+                            document.getElementById("main").appendChild(card);
+                            document.getElementById("btn1").disabled = true;
+
+                    });
+                },
+                error: function (xhr, textStatus, errorThrown) {
+                    console.log('Error in Database');
+                }
+               });
+   }
+
 });
